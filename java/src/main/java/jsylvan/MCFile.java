dcb11e0c71be90b52ef35c2972cd2f84959b54ac
Tom van Dijk <t.vandijk@utwente.nl>
2014-04-30 12:58:39 +0200

    Implemented example model checking program

---
+++ java/src/main/java/jsylvan/MCFile.java
@@ -0,0 +1,68 @@
+package jsylvan;
+
+class MCFile
+{
+    private long vectorSize;
+    private long bitsPerInteger;
+
+    private long initial;
+    private long domain;
+
+    private long[] relations;
+    private long[] relation_domains;
+
+    private native static MCFile fromFile(String filename);
+
+    public long bfs() {
+        long level_counter = 1;
+        long states = initial;
+        long new_states = JSylvan.ref(states);
+        do {
+            System.out.format("Level %d\n", level_counter++);
+            // System.out.format("Level %d: %.0f states\n", level_counter++, JSylvan.satcount(states, domain)); // to do variables
+            long cur_states = new_states;
+            new_states = JSylvan.getFalse();
+            for (int i=0; i<relations.length; i++) {
+                long a = JSylvan.ref(JSylvan.makeNext(cur_states, relations[i], relation_domains[i]));
+                long b = JSylvan.ref(JSylvan.makeIte(states, JSylvan.getFalse(), a)); // b = a - states
+                JSylvan.deref(a);
+                long c = JSylvan.ref(JSylvan.makeOr(new_states, b));
+                JSylvan.deref(b);
+                JSylvan.deref(new_states);
+                new_states = c;
+            }
+            JSylvan.deref(cur_states);
+            long temp = JSylvan.ref(JSylvan.makeOr(states, new_states));
+            JSylvan.deref(states);
+            states = temp;
+        } while (new_states != JSylvan.getFalse());
+        JSylvan.deref(new_states);
+
+        return states;
+    }
+
+    public static void main(String[] args) {
+        if (args.length == 0) {
+            System.out.println("Use: jsylvan.MCFile <filename> [workers]");
+            return;
+        }
+
+        if (args.length == 2) JSylvan.initialize(Integer.parseInt(args[1]), 100000, 30, 26, 4);
+        else JSylvan.initialize(4, 100000, 30, 26, 4);
+
+        MCFile f = MCFile.fromFile(args[0]);
+
+        System.out.format("Read '%s': state vector: %d ints, %d bits per int, %d transition groups\n", args[0], f.vectorSize, f.bitsPerInteger, f.relations.length);
+        System.out.format("Initial states: %d BDD nodes\n", JSylvan.nodecount(f.initial));
+        for (int i=0; i<f.relations.length; i++) System.out.format("Transition %d: %d BDD nodes\n", i, JSylvan.nodecount(f.relations[i]));
+
+        final long t1 = System.nanoTime();
+        long result = f.bfs();
+        final long t2 = System.nanoTime();
+
+        System.out.format("Expired time: %d.%d seconds\n", (t2-t1)/1000000000, (t2-t1)%1000000000);
+
+        System.out.format("Final result: %.0f states\n", JSylvan.satcount(result, f.domain));
+        System.out.format("Final result: %d nodes\n", JSylvan.nodecount(result));
+    }
+}
---
+++ native/src/mc_help.c
@@ -0,0 +1,108 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sylvan.h>
+#include <assert.h>
+#include <jni.h>
+
+JNIEXPORT jobject JNICALL
+Java_jsylvan_MCFile_fromFile(JNIEnv *env, jclass cl, jstring filename)
+{
+    const char *fname = (*env)->GetStringUTFChars(env, filename, 0);
+    FILE *f = fopen(fname, "r");
+
+    if (f == NULL) {
+        // TODO: exception
+        fprintf(stderr, "Cannot open file '%s'!\n", fname);
+        return 0;
+    }
+
+    (*env)->ReleaseStringUTFChars(env, filename, fname);
+
+    size_t vector_size, bits_per_integer;
+
+    // Read domain info
+    assert(fread(&vector_size, sizeof(size_t), 1, f)==1);
+    assert(fread(&bits_per_integer, sizeof(size_t), 1, f)==1);
+
+    BDDVAR *buf = (BDDVAR*)alloca(sizeof(BDDVAR) * bits_per_integer * vector_size * 2);
+
+    // Skip domain
+    fseek(f, sizeof(BDDVAR) * vector_size * bits_per_integer * 2, SEEK_CUR);
+
+    // Read initial state
+    sylvan_serialize_fromfile(f);
+
+    size_t bdd;
+    assert(fread(&bdd, sizeof(size_t), 1, f) == 1);
+    BDD initial = sylvan_ref(sylvan_serialize_get_reversed(bdd));
+
+    // Read domain
+    fseek(f, sizeof(size_t), SEEK_CUR);
+    assert(fread(buf, sizeof(BDDVAR), bits_per_integer * vector_size, f) == bits_per_integer * vector_size);
+    BDD domain = sylvan_ref(sylvan_set_fromarray(buf, bits_per_integer * vector_size));
+
+    // Read transitions
+    int nGrps;
+    assert(fread(&nGrps, sizeof(int), 1, f) == 1);
+
+    BDD *next = (BDD*)alloca(sizeof(BDD) * nGrps);
+    BDD *domains = (BDD*)alloca(sizeof(BDD) * nGrps);
+
+    int i;
+    for (i=0; i<nGrps; i++) {
+        sylvan_serialize_fromfile(f);
+
+        assert(fread(&bdd, sizeof(size_t), 1, f) == 1);
+        next[i] = sylvan_ref(sylvan_serialize_get_reversed(bdd));
+
+        // Read domain
+        size_t v_size;
+        assert(fread(&v_size, sizeof(size_t), 1, f) == 1);
+        assert(fread(buf, sizeof(BDDVAR), v_size * bits_per_integer * 2, f) == v_size * bits_per_integer * 2);
+        domains[i] = sylvan_ref(sylvan_set_fromarray(buf, v_size * bits_per_integer * 2));
+    }
+
+    fclose(f);
+
+    // Convert everything to a nice object
+    jmethodID constructor = (*env)->GetMethodID(env, cl, "<init>", "()V");
+    jobject result = (*env)->NewObject(env, cl, constructor);
+
+    jfieldID fid;
+    jlongArray arr;
+    jlong *dest;
+
+    // set vectorSize field
+    fid = (*env)->GetFieldID(env, cl, "vectorSize", "J");
+    (*env)->SetLongField(env, result, fid, (jlong)vector_size);
+
+    // set bitsPerInteger field
+    fid = (*env)->GetFieldID(env, cl, "bitsPerInteger", "J");
+    (*env)->SetLongField(env, result, fid, (jlong)bits_per_integer);
+
+    // set initial field
+    fid = (*env)->GetFieldID(env, cl, "initial", "J");
+    (*env)->SetLongField(env, result, fid, (jlong)initial);
+
+    // set domain field
+    fid = (*env)->GetFieldID(env, cl, "domain", "J");
+    (*env)->SetLongField(env, result, fid, (jlong)domain);
+
+    // set relations field
+    arr = (*env)->NewLongArray(env, nGrps);
+    dest = (*env)->GetLongArrayElements(env, arr, 0);
+    for (i=0; i<nGrps; i++) dest[i] = next[i];
+    (*env)->ReleaseLongArrayElements(env, arr, dest, 0);
+    fid = (*env)->GetFieldID(env, cl, "relations", "[J");
+    (*env)->SetObjectField(env, result, fid, arr);
+
+    // set domains array
+    arr = (*env)->NewLongArray(env, nGrps);
+    dest = (*env)->GetLongArrayElements(env, arr, 0);
+    for (i=0; i<nGrps; i++) dest[i] = domains[i];
+    (*env)->ReleaseLongArrayElements(env, arr, dest, 0);
+    fid = (*env)->GetFieldID(env, cl, "relation_domains", "[J");
+    (*env)->SetObjectField(env, result, fid, arr);
+
+    return result;
+}
